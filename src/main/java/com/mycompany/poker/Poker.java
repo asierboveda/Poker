f0be1efae02467846b9318e8d915ca2bcdc89406
asier <asier@A>
2024-04-23 16:04:55 +0200

    Poker

--- src/main/java/com/mycompany/poker/Poker.java
+++ src/main/java/com/mycompany/poker/Poker.java
@@ -13,5 +13,6 @@ public class Poker {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola");
+         System.out.println("hola 2");
     }
 }
